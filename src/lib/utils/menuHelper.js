d768bfeea8cd5862439ac0452211fba4d13daf41
Trpquo <trpimir.jezic@gmail.com>
2022-01-07 21:25:05 +0100

    Test nakon što posts.json nije radio na netlify-u.

--- src/lib/utils/menuHelper.js
+++ src/lib/utils/menuHelper.js
@@ -1,12 +1,29 @@
+import { get } from '../../routes/posts.json.js'
+
 export default async ( ) => {
+
+    let gotIt = false, response = null;
 
     try {
         const posts = await fetch( '/posts.json' )
         const { menu } = await posts.json()
-        return menu 
+        gotIt = true
+        response = menu 
     }
     catch ({ message }) {
         console.error
     }
+
+
+    if ( !gotIt ) {
+        const { body } = await get()
+        console.log( "Get in place of fetch!" )
+        return body
+    } else {
+        
+        console.log( "Fetch in place of get!" )
+        return response
+    }
+    
 
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
+<svelte:head>
+    <title>Gipsoteka | Otvor</title>
+</svelte:head>
+
 <h1>Prva</h1>
 <p>Nisi još ništo vidio</p>
