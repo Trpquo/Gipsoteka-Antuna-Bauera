09f007df5a867c8e9ccea1efe5b3c525e1b2cb7b
Trpquo <trpimir.jezic@gmail.com>
2022-01-08 00:42:22 +0100

    "Dosta za danas."

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
     import Article from '$lib/components/Article.svelte'
 </script>
 
-<Article title="Otvor" chapter="100 god.">
+<Article title="Otvor" chapter="110 god.">
     <h1>Dobrodošli!</h1>
     <h3>Na žalost, još nemamo naslovnicu.</h3>
     <p>Probajte izbornik.</p>
